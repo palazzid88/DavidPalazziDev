d1a168746d5b104a800889bc1026cae38113117a
palazzid88 <palazzid88@gmail.com>
2024-09-22 22:42:20 -0300

    se corrige error en Navbar

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,5 @@
 'use client';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 import { useState } from 'react';
