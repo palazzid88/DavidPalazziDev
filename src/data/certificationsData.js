440907d7d9c257ae789cff89ca7c2c7e007dcf35
palazzid88 <palazzid88@gmail.com>
2024-10-18 17:15:02 -0300

    se añade certificacion

--- src/data/certificationsData.js
+++ src/data/certificationsData.js
@@ -3,6 +3,6 @@ export const certifications = [
   { name: 'Backend Express.js', src: '/images/certifications/programacionBackend.png' },
   { name: 'React.js', src: '/images/certifications/react.png' },
   { name: 'JavaScript', src: '/images/certifications/javascript.png' },
-  { name: 'Desarrollo Web (SN)', src: '/images/certifications/dwebsn.png' },
+  { name: 'Desarrollo Web (SN)', src: '/images/certifications/desarrolloWebSn.png' },
   { name: 'Desarrollo Web', src: '/images/certifications/desarrolloWeb.png' },
   ];
